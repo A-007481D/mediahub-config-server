9ba908aad4e6046b543ea606de47285d6358da9e
A-007481D <labidabdelmalek@gmail.com>
2026-03-03 22:18:18 +0000

    CHORE: initialize and setup the config server properties

---
+++ src/test/java/com/mediahub/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediahub.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
